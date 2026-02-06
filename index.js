1066c5bb701666c9515ae8352b0229904c3e2b1b
abhishek1531 <abhi.singh15dec@gmail.com>
2026-02-06 11:26:23 +0530

    Added utils functions and fixed index.js

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-import fs from "fs";
+// import fs from "fs";
+// import path from "path";
 
-// 1️⃣ WRITE (overwrite / create)
-fs.writeFileSync("demo.txt", "Hello, this is written using writeFileSync\n");
+// fs.writeFileSync("demo.txt", "Hello, this is written using writeFileSync\n");
 
-// 2️⃣ APPEND (add at end)
-fs.appendFileSync("demo.txt", "This line is added using appendFileSync\n");
+// fs.appendFileSync("demo.txt", "This line is added using appendFileSync\n");
 
-// 3️⃣ READ (read file content)
-const data = fs.readFileSync("demo.txt", "utf-8");
+// const data = fs.readFileSync("demo.txt", "utf-8");
 
-console.log(data);
+// console.log(data);
+
+
+// const filePath = "C:\\Users\\Abhishek Singh\\Downloads\\Cloud Assign 2 .pdf";
+
+// const dynamicPath = path.join("C:","Users","Abhishek Singh","Downloads","Cloud Assign 2.pdf");
+// console.log("Dyanmic Path",dynamicPath);
+// fs.readFile(filePath,"utf-8",(err,data)=>{
+//     console.log(data);
+// });
+
+//---------------------------------------------------------------------------------------------------------------------------
+
+// const {addd,multii} = require("./utils")
+// // const functions = require("./utils")
+// // const multi = require("./utils")
+
+// functions = {
+//     addd,
+//     multii
+// }
+// const {addd,multii} = functions;
+
+// // console.log(functions['add'](1,3));
+// // console.log(functions['multi'](6,5));
+
+
+// console.log(addd(1,3));
+// console.log(multii(6,5));
+
+// Q) functions declare then export want to write our own function & declare it, and how to export it , how to import .
+
+const { isEven, square } = require("./utils");
+
+console.log(isEven(10));   
+console.log(isEven(7));    
+
+console.log(square(5));    
+console.log(square(9));    
+
---
+++ utils.js
@@ -0,0 +1,12 @@
+const isEven = (num) => {
+    return num % 2 === 0;
+};
+
+const square = (num) => {
+    return num * num;
+};
+
+module.exports = {
+    isEven,
+    square
+};
